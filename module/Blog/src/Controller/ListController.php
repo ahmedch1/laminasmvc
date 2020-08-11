4bde021d10563213fd9644ce92c4954562a15fc8
Ahmed Ch <ahmedchouihi@gmail.com>
2020-08-11 16:29:51 +0100

    Models and Service Manager

---
+++ module/Blog/src/Controller/ListController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Blog\Controller;
+
+use Blog\Model\PostRepositoryInterface;
+use Laminas\Mvc\Controller\AbstractActionController;
+// Add the following import statement:
+use Laminas\View\Model\ViewModel;
+
+class ListController extends AbstractActionController
+{
+    /**
+     * @var PostRepositoryInterface
+     */
+    private $postRepository;
+
+    public function __construct(PostRepositoryInterface $postRepository)
+    {
+        $this->postRepository = $postRepository;
+    }
+
+    // Add the following method:
+    public function indexAction()
+    {
+        return new ViewModel([
+            'posts' => $this->postRepository->findAllPosts(),
+        ]);
+    }
+}
