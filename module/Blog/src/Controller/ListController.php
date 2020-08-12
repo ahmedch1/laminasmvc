f2a22143d859841b7144b2551c45c71458f65cbe
Ahmed Ch <ahmedchouihi@gmail.com>
2020-08-12 18:48:06 +0100

    Understanding the Router

--- module/Blog/src/Controller/ListController.php
+++ module/Blog/src/Controller/ListController.php
@@ -2,6 +2,7 @@
 
 namespace Blog\Controller;
 
+use InvalidArgumentException;
 use Blog\Model\PostRepositoryInterface;
 use Laminas\Mvc\Controller\AbstractActionController;
 // Add the following import statement:
@@ -24,6 +25,19 @@ class ListController extends AbstractActionController
     {
         return new ViewModel([
             'posts' => $this->postRepository->findAllPosts(),
+        ]);
+    }
+    public function detailAction(){
+        $id = $this->params()->fromRoute('id');
+
+        try {
+            $post = $this->postRepository->findPost($id);
+        } catch (\InvalidArgumentException $ex) {
+            return $this->redirect()->toRoute('blog');
+        }
+
+        return new ViewModel([
+            'post' => $post,
         ]);
     }
 }
