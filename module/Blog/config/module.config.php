29d27adadaab85feeaa5559f28358ec74966fef6
Ahmed Ch <ahmedchouihi@gmail.com>
2020-08-12 22:08:37 +0100

    Making Use of Forms and Fieldsets

--- module/Blog/config/module.config.php
+++ module/Blog/config/module.config.php
@@ -10,45 +10,63 @@ return [
     'service_manager' => [
         'aliases' => [
             Model\PostRepositoryInterface::class => Model\LaminasDbSqlRepository::class,
+            Model\PostCommandInterface::class => Model\LaminasDbSqlCommand::class,
+
         ],
         'factories' => [
             Model\PostRepository::class => InvokableFactory::class,
             Model\LaminasDbSqlRepository::class => Factory\LaminasDbSqlRepositoryFactory::class,
+            Model\PostCommand::class => InvokableFactory::class,
+            Model\LaminasDbSqlCommand::class => Factory\LaminasDbSqlCommandFactory::class,
         ],
     ],
     'controllers' => [
         'factories' => [
             Controller\ListController::class => Factory\ListControllerFactory::class,
+            Controller\WriteController::class => Factory\WriteControllerFactory::class,
         ],
     ],
-    'router'          => [
+    'router' => [
         'routes' => [
             'blog' => [
                 'type' => Literal::class,
                 'options' => [
-                    'route'    => '/blog',
+                    'route' => '/blog',
                     'defaults' => [
                         'controller' => Controller\ListController::class,
-                        'action'     => 'index',
+                        'action' => 'index',
                     ],
                 ],
                 'may_terminate' => true,
-                'child_routes'  => [
+                'child_routes' => [
                     'detail' => [
                         'type' => Segment::class,
                         'options' => [
-                            'route'    => '/:id',
+                            'route' => '/:id',
                             'defaults' => [
                                 'action' => 'detail',
                             ],
                             'constraints' => [
-                                'id' => '[1-9]\d*',
+                                'id' => '\d+',
+                            ],
+                        ],
+                    ],
+
+                    // Add the following route:
+                    'add' => [
+                        'type' => Literal::class,
+                        'options' => [
+                            'route' => '/add',
+                            'defaults' => [
+                                'controller' => Controller\WriteController::class,
+                                'action' => 'add',
                             ],
                         ],
                     ],
                 ],
             ],
         ],
+
     ],
     'view_manager' => [
         'template_path_stack' => [
